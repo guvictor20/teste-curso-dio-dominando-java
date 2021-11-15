f958284893960d09976f66be197d61b8d384fd49
guvictor20 <gustavo.rodolfovictor@gmail.com>
2021-11-15 11:53:49 -0300

    Update program

---
+++ src/resolveta_tabuada/ResolveTabuada.java
@@ -0,0 +1,22 @@
+package resolveta_tabuada;
+
+import javax.swing.JOptionPane;
+
+public class ResolveTabuada {
+	
+	public static void main(String[] args) {
+		
+		int fator1;
+		int fator2;
+		int produto;
+		
+		fator1 = Integer.parseInt(JOptionPane.showInputDialog("Qual tabuada quer exibir?"));
+		fator2 = Integer.parseInt(JOptionPane.showInputDialog("Até quanto quer exibir o resultado?"));
+		
+		for (int i = 0; i <= fator2; i++) {
+			produto = fator1 * i;
+			System.out.println(fator1+" x "+i+" = "+produto);
+		}
+	}
+
+}
